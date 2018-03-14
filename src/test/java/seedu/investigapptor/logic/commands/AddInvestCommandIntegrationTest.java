45b39219712af70212a8bc7d61119c314937f076
pkaijun <kai_jun96@hotmail.com>
2018-03-14 15:59:46 +0800

    Refactored "addCommand" to "addInvestCommand"

--- src/test/java/seedu/investigapptor/logic/commands/AddInvestCommandIntegrationTest.java
+++ src/test/java/seedu/investigapptor/logic/commands/AddInvestCommandIntegrationTest.java
@@ -16,9 +16,9 @@ import seedu.investigapptor.model.person.Person;
 import seedu.investigapptor.testutil.PersonBuilder;
 
 /**
- * Contains integration tests (interaction with the Model) for {@code AddCommand}.
+ * Contains integration tests (interaction with the Model) for {@code AddInvestCommand}.
  */
-public class AddCommandIntegrationTest {
+public class AddInvestCommandIntegrationTest {
 
     private Model model;
 
@@ -35,20 +35,20 @@ public class AddCommandIntegrationTest {
         expectedModel.addPerson(validPerson);
 
         assertCommandSuccess(prepareCommand(validPerson, model), model,
-                String.format(AddCommand.MESSAGE_SUCCESS, validPerson), expectedModel);
+                String.format(AddInvestCommand.MESSAGE_SUCCESS, validPerson), expectedModel);
     }
 
     @Test
     public void execute_duplicatePerson_throwsCommandException() {
         Person personInList = model.getInvestigapptor().getPersonList().get(0);
-        assertCommandFailure(prepareCommand(personInList, model), model, AddCommand.MESSAGE_DUPLICATE_PERSON);
+        assertCommandFailure(prepareCommand(personInList, model), model, AddInvestCommand.MESSAGE_DUPLICATE_PERSON);
     }
 
     /**
-     * Generates a new {@code AddCommand} which upon execution, adds {@code person} into the {@code model}.
+     * Generates a new {@code AddInvestCommand} which upon execution, adds {@code person} into the {@code model}.
      */
-    private AddCommand prepareCommand(Person person, Model model) {
-        AddCommand command = new AddCommand(person);
+    private AddInvestCommand prepareCommand(Person person, Model model) {
+        AddInvestCommand command = new AddInvestCommand(person);
         command.setData(model, new CommandHistory(), new UndoRedoStack());
         return command;
     }
